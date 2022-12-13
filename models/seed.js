eed5ced0c5f52fde4f6467417f9d558cd6b54497
Alenmnoian4 <alenmnoian4@gmail.com>
2022-12-13 18:34:27 -0500

    added

--- models/seed.js
+++ models/seed.js
@@ -4,7 +4,7 @@ const Vehicle = require("./vehicle")
 
 mongoose.connection.on("open", () => {
     const startingVehicles =  [
-        { company: "Bugatti", model: "Chiro Super Sport" ,color: "orange", horsepower: "1,577", cost: "$3,825,000"  , inStock: true },
+        { company: "Bugatti", model: "Chiron Super Sport" ,color: "orange", horsepower: "1,577", cost: "$3,825,000", img: "https://i.pinimg.com/originals/b4/73/a8/b473a81fd3e33f7e41f9a4648d6f18c0.jpg"  , inStock: true },
         { company: "Lamborghini", model: "Aventador" ,color: "purple", horsepower: "769" , cost: "$546,847", inStock: true },
         { company: "Ferrari", model: "SF90 Stradale" ,color: "orange", horsepower: "1,000", cost: "$625,000" , inStock: true },
         { company: "Porsche", model: "911 GT3 RS" ,color: "red", horsepower: "518", cost: "$314,000" , inStock: true },
